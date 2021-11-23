31c7013866ca611d8e179c7135ef4572f9106f09
Zhang XiaoFei <1326336909@qq.com>
2021-11-23 09:22:19 +0800

    入库流程开发

--- src/lang/module/platform/inventory/zh.js
+++ src/lang/module/platform/inventory/zh.js
@@ -29,7 +29,7 @@ export const inventory = {
     basic_info: '设备信息',
     inventory_info: '盘点信息',
     resource_info: '资料信息',
-    abnormal_info: '异常信息',
+    abnormal_info: '签收信息',
     finish_info: '完成信息',
 
     write: '填写信息',
