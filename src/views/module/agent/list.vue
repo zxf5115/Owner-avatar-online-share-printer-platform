7427da45d4bc72977730c5bfac0eb0991ab47199
Zhang <1326336909@qq.com>
2022-01-14 09:51:26 +0800

    申请文书宽度

--- src/views/module/agent/list.vue
+++ src/views/module/agent/list.vue
@@ -155,7 +155,7 @@
             </template>
           </el-table-column>
 
-          <el-table-column :label="$t('common.handle')" fixed="right" width="350">
+          <el-table-column :label="$t('common.handle')" fixed="right" width="320">
             <template slot-scope="scope">
               <el-button v-if="isAuth('module:agent:view')" type="info" icon="el-icon-view" @click="$router.push({name: 'module_agent_view', query: {id: scope.row.id}})">
                 {{ $t('common.view') }}
